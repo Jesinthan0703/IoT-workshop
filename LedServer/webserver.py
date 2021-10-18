02b60cca49f9f4546401de592cd8545186a74c90
Jesinthan0703 <jesinthan.23cs@licet.ac.in>
2021-10-18 19:34:53 +0530

    LED Webserver controller

---
+++ LedServer/webserver.py
@@ -0,0 +1,48 @@
+# Complete project details at https://RandomNerdTutorials.com
+
+import machine
+led = machine.Pin(14, machine.Pin.OUT)   #D5 pin
+
+def web_page():
+
+  if led.value() == 1:
+    gpio_state="ON"
+  else:
+    gpio_state="OFF"
+  
+  html = """<html><head> <title>LED CONTROLLER</title> <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="data:,"> <style>html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
+  h1{color: #0F3376; padding: 2vh;}p{font-size: 1.5rem;}.button{display: inline-block; background-color: #e7bd3b; border: none; 
+  border-radius: 4px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
+  .button2{background-color: #4286f4;}</style></head><body> <h1>LED CONTROLLER</h1> 
+  <p>GPIO state: <strong>""" + gpio_state + """</strong></p><p><a href="/?led=on"><button class="button">ON</button></a></p>
+  <p><a href="/?led=off"><button class="button button2">OFF</button></a></p></body></html>"""
+  return html
+
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('192.168.4.1', 60))
+s.listen(1)
+
+print("Listening...")
+
+while True:
+  conn, addr = s.accept()
+  print('Got a connection from %s' % str(addr))
+  request = conn.recv(1024)
+  request = str(request)
+
+  led_on = request.find('/?led=on')
+  led_off = request.find('/?led=off')
+  if led_on == 6:
+    print('LED ON')
+    led.on()
+  if led_off == 6:
+    print('LED OFF')
+    led.off()
+  response = web_page()
+  conn.send('HTTP/1.1 200 OK\n')
+  conn.send('Content-Type: text/html\n')
+  conn.send('Connection: close\n\n')
+  conn.sendall(response)
+  conn.close()
